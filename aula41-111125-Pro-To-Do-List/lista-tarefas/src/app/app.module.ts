06ff202c0b2e2e3b9af533aa3cd4ffa7b7d9e113
Magno V Gomes <magnovgomes74@gmail.com>
2025-12-09 17:38:10 -0300

    Aula 41 continuação da aula 40 sobre o framework Angular

---
+++ aula41-111125-Pro-To-Do-List/lista-tarefas/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; 
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TarefasComponent } from './tarefas/tarefas.component';
+
+@NgModule({
+  declarations:[
+    AppComponent,
+    TarefasComponent
+  ],
+  imports:[
+    BrowserModule,
+    FormsModule
+  ],
+  providers:[],
+  bootstrap:[AppComponent]
+})
+export class AppModule {}
