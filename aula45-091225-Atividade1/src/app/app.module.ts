fb0a5fac8f96572f09e8ca1b7ef67ef2cd2e5ee9
Magno V Gomes <magnovgomes74@gmail.com>
2025-12-12 11:19:40 -0300

    Criando 4 components: inicio, login, menu e ropade

--- aula45-091225-Atividade1/src/app/app.module.ts
+++ aula45-091225-Atividade1/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { MenuComponent } from './menu/menu.component';
+import { LoginComponent } from './login/login.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    MenuComponent,
+    LoginComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
